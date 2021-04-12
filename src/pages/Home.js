772c56174aca6425d6e8b9309c7163c5850f67f3
keithchasen <keithchasen89@gmail.com>
2021-04-12 18:22:46 +0300

    fetching posts from server

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { Grid } from 'semantic-ui-react'
 
 function Home() {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  const content =
+    loading ?
+      <h1>Posts are loading...</h1> :
+      data.getPosts && data.getPosts.map(post =>  <Grid.Column key={post.id}><PostCard post={post} /></Grid.Column>);
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Grid columns={3} divided>
+      <Grid.Row>
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        { content }
+      </Grid.Row>
+    </Grid>
   )
 }
+
+const FETCH_POSTS_QUERY = gql`
+    {
+        getPosts {
+            id
+            body
+            createdAt
+            username
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id
+                username
+                createdAt
+                body
+            }
+        }
+    }
+`;
 
 export default Home;
