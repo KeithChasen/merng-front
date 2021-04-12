0bc6880577498b2617fa11e36ab7677a16d98dbb
keithchasen <keithchasen89@gmail.com>
2021-04-12 15:44:20 +0300

    basic nav bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <Router>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
-      <Route exact path='/register' component={Register} />
-    </Router>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Container>
+      </Router>
   );
 }
 
